db1ea8b8989c8f20ce2ba774cd721915608b7d68
shakeel7521951 <shakeel7521951@gmail.com>
2026-02-28 10:48:08 +0500

    feat: initialize NestJS application with basic structure and modules
    - Added package.json with dependencies and scripts for NestJS application
    - Created AppController, AppService, and their respective tests
    - Implemented AuthModule and UserModule with controllers and services
    - Set up Mongoose for MongoDB connection and defined User schema
    - Added main entry point for the application
    - Included Jest configuration for unit and e2e testing
    - Established basic e2e test for the root endpoint
    - Configured TypeScript settings for the project

---
+++ src/user/schemas/user.schema.ts
@@ -0,0 +1,23 @@
+import { Prop, Schema, SchemaFactory } from "@nestjs/mongoose";
+import { Document } from "mongoose";
+
+@Schema({timestamps: true})
+export class User extends Document{
+    @Prop({required:true})
+    name!: string;
+
+    @Prop({required:true, unique:true})
+    email!: string;
+
+    @Prop({required:true})
+    password!: string;
+
+    @Prop()
+    refreshToken?: string;
+
+    @Prop({required:true,default:'user'})
+    role!: string;
+
+}
+
+export const UserSchema = SchemaFactory.createForClass(User);
